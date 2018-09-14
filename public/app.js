9ca9d4b4d32a29973f1c27ddb45bd9364dd3c56f
Aniruddha Murali <aniruddha.murali@gmail.com>
2018-09-14 16:04:58 -0400

    Navigation and buttons

--- public/app.js
+++ public/app.js
@@ -1,4 +1,39 @@
 $("a").click(function() {
   $("a").removeClass("active");
   $(this).addClass("active");
+  if ($(this).text() == "About") {
+    $(".about").show();
+    $(".awards").hide();
+    $(".experience").hide();
+    $(".blog").hide();
+    $(".contact").hide();
+  }
+  else if ($(this).text() == "Awards") {
+    $(".awards").show();
+    $(".about").hide();
+    $(".experience").hide();
+    $(".blog").hide();
+    $(".contact").hide();
+  }
+  else if ($(this).text() == "Experience") {
+    $(".experience").show();
+    $(".about").hide();
+    $(".awards").hide();
+    $(".blog").hide();
+    $(".contact").hide();
+  }
+  else if ($(this).text() == "Blog") {
+    $(".blog").show();
+    $(".about").hide();
+    $(".awards").hide();
+    $(".experience").hide();
+    $(".contact").hide();
+  }
+  else if ($(this).text() == "Contact") {
+    $(".contact").show();
+    $(".about").hide();
+    $(".awards").hide();
+    $(".experience").hide();
+    $(".blog").hide();
+  }
 });
