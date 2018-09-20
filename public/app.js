df46d83e188722e334d19a003e03546a0b91a118
Aniruddha Murali <aniruddha.murali@gmail.com>
2018-09-20 17:50:12 -0400

    Added contact form and modified sidebar

--- public/app.js
+++ public/app.js
@@ -1,56 +1,61 @@
 $(document).ready(function(){
-    $('.ui .item').on('click', function() {
-        //$('.ui .item').removeClass('active');
-        console.log($(this).text());
-        $("a").removeClass('active');
-        $(this).addClass('active');
-        if ($(this).text() == "Home") {
-          $(".home").show();
-          $(".about").hide();
-          $(".awards").hide();
-          $(".experience").hide();
-          $(".blog").hide();
-          $(".contact").hide();
-        }
-        if ($(this).text() == "About") {
-          $(".about").show();
-          $(".home").hide();
-          $(".awards").hide();
-          $(".experience").hide();
-          $(".blog").hide();
-          $(".contact").hide();
-        }
-        else if ($(this).text() == "Awards") {
-          $(".awards").show();
-          $(".home").hide();
-          $(".about").hide();
-          $(".experience").hide();
-          $(".blog").hide();
-          $(".contact").hide();
-        }
-        else if ($(this).text() == "Experience") {
-          $(".experience").show();
-          $(".home").hide();
-          $(".about").hide();
-          $(".awards").hide();
-          $(".blog").hide();
-          $(".contact").hide();
-        }
-        else if ($(this).text() == "Blog") {
-          $(".blog").show();
-          $(".home").hide();
-          $(".about").hide();
-          $(".awards").hide();
-          $(".experience").hide();
-          $(".contact").hide();
-        }
-        else if ($(this).text() == "Contact") {
-          $(".contact").show();
-          $(".home").hide();
-          $(".about").hide();
-          $(".awards").hide();
-          $(".experience").hide();
-          $(".blog").hide();
-        }
-    });
+  $(".home").show();
+  $(".about").hide();
+  $(".awards").hide();
+  $(".experience").hide();
+  $(".blog").hide();
+  $(".contact").hide();
+
+  $('.ui .item').on('click', function() {
+    $("a").removeClass('active');
+    $(this).addClass('active');
+    if ($(this).text() == "Home") {
+      $(".home").show();
+      $(".about").hide();
+      $(".awards").hide();
+      $(".experience").hide();
+      $(".blog").hide();
+      $(".contact").hide();
+    }
+    if ($(this).text() == "About") {
+      $(".about").show();
+      $(".home").hide();
+      $(".awards").hide();
+      $(".experience").hide();
+      $(".blog").hide();
+      $(".contact").hide();
+    }
+    else if ($(this).text() == "Awards") {
+      $(".awards").show();
+      $(".home").hide();
+      $(".about").hide();
+      $(".experience").hide();
+      $(".blog").hide();
+      $(".contact").hide();
+    }
+    else if ($(this).text() == "Experience") {
+      $(".experience").show();
+      $(".home").hide();
+      $(".about").hide();
+      $(".awards").hide();
+      $(".blog").hide();
+      $(".contact").hide();
+    }
+    else if ($(this).text() == "Blog") {
+      $(".blog").show();
+      $(".home").hide();
+      $(".about").hide();
+      $(".awards").hide();
+      $(".experience").hide();
+      $(".contact").hide();
+    }
+    else if ($(this).text() == "Contact") {
+      $(".contact").show();
+      $(".home").hide();
+      $(".about").hide();
+      $(".awards").hide();
+      $(".experience").hide();
+      $(".blog").hide();
+    }
+  });
 });
