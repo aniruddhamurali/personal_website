d65bc26af0d63b4e2ed900725afd6dbda3df2999
Aniruddha Murali <aniruddha.murali@gmail.com>
2018-09-20 14:02:07 -0400

    Added Semantic UI framework and modified sidebar

--- public/app.js
+++ public/app.js
@@ -1,39 +1,56 @@
-$("a").click(function() {
-  $("a").removeClass("active");
-  $(this).addClass("active");
-  if ($(this).text() == "About") {
-    $(".about").show();
-    $(".awards").hide();
-    $(".experience").hide();
-    $(".blog").hide();
-    $(".contact").hide();
-  }
-  else if ($(this).text() == "Awards") {
-    $(".awards").show();
-    $(".about").hide();
-    $(".experience").hide();
-    $(".blog").hide();
-    $(".contact").hide();
-  }
-  else if ($(this).text() == "Experience") {
-    $(".experience").show();
-    $(".about").hide();
-    $(".awards").hide();
-    $(".blog").hide();
-    $(".contact").hide();
-  }
-  else if ($(this).text() == "Blog") {
-    $(".blog").show();
-    $(".about").hide();
-    $(".awards").hide();
-    $(".experience").hide();
-    $(".contact").hide();
-  }
-  else if ($(this).text() == "Contact") {
-    $(".contact").show();
-    $(".about").hide();
-    $(".awards").hide();
-    $(".experience").hide();
-    $(".blog").hide();
-  }
+$(document).ready(function(){
+    $('.ui .item').on('click', function() {
+        //$('.ui .item').removeClass('active');
+        console.log($(this).text());
+        $("a").removeClass('active');
+        $(this).addClass('active');
+        if ($(this).text() == "Home") {
+          $(".home").show();
+          $(".about").hide();
+          $(".awards").hide();
+          $(".experience").hide();
+          $(".blog").hide();
+          $(".contact").hide();
+        }
+        if ($(this).text() == "About") {
+          $(".about").show();
+          $(".home").hide();
+          $(".awards").hide();
+          $(".experience").hide();
+          $(".blog").hide();
+          $(".contact").hide();
+        }
+        else if ($(this).text() == "Awards") {
+          $(".awards").show();
+          $(".home").hide();
+          $(".about").hide();
+          $(".experience").hide();
+          $(".blog").hide();
+          $(".contact").hide();
+        }
+        else if ($(this).text() == "Experience") {
+          $(".experience").show();
+          $(".home").hide();
+          $(".about").hide();
+          $(".awards").hide();
+          $(".blog").hide();
+          $(".contact").hide();
+        }
+        else if ($(this).text() == "Blog") {
+          $(".blog").show();
+          $(".home").hide();
+          $(".about").hide();
+          $(".awards").hide();
+          $(".experience").hide();
+          $(".contact").hide();
+        }
+        else if ($(this).text() == "Contact") {
+          $(".contact").show();
+          $(".home").hide();
+          $(".about").hide();
+          $(".awards").hide();
+          $(".experience").hide();
+          $(".blog").hide();
+        }
+    });
 });
