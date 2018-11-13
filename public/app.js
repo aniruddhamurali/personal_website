609e576e5fcc3ecb6208c97e99abcde1b1805719
Aniruddha Murali <aniruddha.murali@gmail.com>
2018-11-13 11:45:40 -0500

    About section and read contact form

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  $(".home").show();
-  $(".about").hide();
+  $(".about").show();
+  $(".portfolio").hide();
   $(".awards").hide();
   $(".experience").hide();
   $(".blog").hide();
@@ -9,17 +9,17 @@ $(document).ready(function(){
   $('.ui .item').on('click', function() {
     $("a").removeClass('active');
     $(this).addClass('active');
-    if ($(this).text() == "Home") {
-      $(".home").show();
-      $(".about").hide();
+    if ($(this).text() == "About") {
+      $(".about").show();
+      $(".portfolio").hide();
       $(".awards").hide();
       $(".experience").hide();
       $(".blog").hide();
       $(".contact").hide();
     }
-    if ($(this).text() == "About") {
-      $(".about").show();
-      $(".home").hide();
+    if ($(this).text() == "Portfolio") {
+      $(".portfolio").show();
+      $(".about").hide();
       $(".awards").hide();
       $(".experience").hide();
       $(".blog").hide();
@@ -27,35 +27,60 @@ $(document).ready(function(){
     }
     else if ($(this).text() == "Awards") {
       $(".awards").show();
-      $(".home").hide();
       $(".about").hide();
+      $(".portfolio").hide();
       $(".experience").hide();
       $(".blog").hide();
       $(".contact").hide();
     }
     else if ($(this).text() == "Experience") {
       $(".experience").show();
-      $(".home").hide();
       $(".about").hide();
+      $(".portfolio").hide();
       $(".awards").hide();
       $(".blog").hide();
       $(".contact").hide();
     }
     else if ($(this).text() == "Blog") {
       $(".blog").show();
-      $(".home").hide();
       $(".about").hide();
+      $(".portfolio").hide();
       $(".awards").hide();
       $(".experience").hide();
       $(".contact").hide();
     }
     else if ($(this).text() == "Contact") {
       $(".contact").show();
-      $(".home").hide();
       $(".about").hide();
+      $(".portfolio").hide();
       $(".awards").hide();
       $(".experience").hide();
       $(".blog").hide();
     }
   });
+
+});
+
+var mandrill = require('node-mandrill')('HFBmP6jNtGU6yd-aHiYiDQ');
+
+function sendEmail (_email, _subject, _message) {
+  mandrill('/messages/send', {
+    message: {
+      to: [{email: 'aniruddha.murali@gmail.com', name: "Aniruddha Murali"}],
+      from_email: _email,
+      subject: _subject,
+      text: _message
+    }
+  }, function(error, response){
+    if (error) console.log(error);
+    else console.log(response);
+  });
+}
+
+
+
+$('.ui .submit').on('click', function() {
+  var name = document.getElementsByTagName("input")[0].value;
+  var email = document.getElementsByTagName("input")[1].value;
+  var message = document.getElementsByTagName("input")[2].value;
 });
